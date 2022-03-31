e1910042226ffac6c279df8634cf5410585ba1f6
André Kuratomi <andrekuratomi@gmail.com>
2022-03-31 19:05:28 -0300

    feat: appointments for tomorrow route drafted

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,12 +5,14 @@ from .views import (
     SpecificAppointmentView,
     SpecificProfessionalView,
     NotFinishedAppointmentView,
+    TomorrowAppointmentView,
 )
 
 urlpatterns = [
     path('appointments/', CreateAppointment.as_view()),
-    path('appointments/professional/<str:council_number>/', SpecificProfessionalView.as_view()),
     path('appointments/<str:appointment_id>/', SpecificAppointmentView.as_view()),
     path('appointments/patient/<str:cpf>/', SpecificPatientView.as_view()),
+    path('appointments/professional/<str:council_number>/', SpecificProfessionalView.as_view()),
     path('appointments/open/', NotFinishedAppointmentView.as_view()),
+    path('appointments/tomorrow/<str:council_number>', TomorrowAppointmentView.as_view()),
 ]
